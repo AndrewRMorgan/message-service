9f1e3a9a7aeb1ee62b62e6c64715d1530f2c53aa
AndrewRMorgan <andrew.r.morgan@gmail.com>
2017-10-05 17:46:44 +0100

    make it better

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
 	"log"
 	"math/rand"
@@ -62,10 +61,8 @@ func postMessageHandler(w http.ResponseWriter, r *http.Request) {
 	check(err)
 
 	response = Response{id: id}
-	js, _ := json.Marshal(response)
-	w.Header().Set("Content-Type", "application/json")
 
-	fmt.Fprintf(w, "%+v\n", js)
+	fmt.Fprintf(w, "%+v\n", response)
 }
 
 func getMessageHandler(w http.ResponseWriter, r *http.Request) {
