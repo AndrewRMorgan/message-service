f567fb3cd392df816bf3214961f0411757450ce1
AndrewRMorgan <andrew.r.morgan@gmail.com>
2017-10-05 16:36:58 +0100

    make it better

--- main.go
+++ main.go
@@ -45,6 +45,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 func postMessageHandler(w http.ResponseWriter, r *http.Request) {
 	var message string
 	//b, _ := ioutil.ReadAll(r.Body)
+	err := r.ParseForm()
 	fmt.Fprintf(w, "Request: %v\n", r.Form)
 
 	id := random(0, 99999)
